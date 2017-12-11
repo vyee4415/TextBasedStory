9ba195ed78e7201ae7f048a4b9ff41328f5061cc
BT_1N3_08 <BT_1N3_08@BT_1N3_08.bktech.local>
2017-12-11 09:17:20 -0500

    woo

--- TextBasedStory/src/RunRoom.java
+++ TextBasedStory/src/RunRoom.java
@@ -1,19 +1,30 @@
-
-public class RunRoom extends Room{
-
-	public RunRoom(int x, int y) {
-		super(x, y);
-		// TODO Auto-generated constructor stub
-	}
-	@Override
-	public void enterRoom(Person x) {
-		// TODO Auto-generated method stub
-		occupant = x;
-		x.setxLoc(this.xLoc);
-		x.setyLoc(this.yLoc);
-		System.out.println("YOURE STUCK");
-		System.out.println("ANSWER THIS MATH PROBLEM TO PASS!");
-		System.out.println(Math.random()*10+Math.random()*20);
-	}
-	
-}
+//import java.util.Scanner;
+//
+//public class RunRoom extends Room{
+//	public RunRoom(int x, int y) {
+//		super(x, y);
+//		// TODO Auto-generated constructor stub
+//	}
+//	@Override
+//	public void enterRoom(Person x) {
+//		// TODO Auto-generated method stub
+//		occupant = x;
+//		x.setxLoc(this.xLoc);
+//		x.setyLoc(this.yLoc);
+//		System.out.println("SOLVE THIS PROBLEM TO GO TO THE NEXT LEVEL!");
+//		problem();
+//	}
+//	public static int problem() {
+//		System.out.print(9);
+//		System.out.print(" x ");
+//		System.out.print(3);
+//		System.out.println(" = ");
+//		System.out.println("Enter a number: ");
+//		x = user_input.next();
+//		if(x != 27) {
+//			
+//		}
+//	}
+//
+//}
+//                                              
--- TextBasedStory/src/Runner.java
+++ TextBasedStory/src/Runner.java
@@ -22,10 +22,17 @@ public class Runner {
 		int x = (int)(Math.random()*building.length);
 		int y = (int)(Math.random()*building.length);
 		building[x][y] = new WinningRoom(x, y);
+		
+		//Create a weird room
 		x = (int)(Math.random()*building.length);
 		y = (int)(Math.random()*building.length);
 		building[x][y] = new WeirdRoom(x, y);
-		 
+		
+//		//Create a math room
+//		x = (int)(Math.random()*building.length);
+//		y = (int)(Math.random()*building.length);
+//		building[x][y] = new RunRoom(x, y);
+		
 		 //Setup player 1 and the input scanner
 		Person player1 = new Person("FirstName", "FamilyName", 0,0);
 		building[0][0].enterRoom(player1);
